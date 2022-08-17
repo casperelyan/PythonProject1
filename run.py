f12ff6a803c6bc3da5b2b9f0496e7385c9f37063
Lucas <4h4photography@gmail.com>
2022-08-17 15:27:37 +0000

    Question add

--- run.py
+++ run.py
@@ -2,7 +2,18 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+name = input("What's your name: ")
+print("Welcome", name, "to Flags of the World Quiz! ")
+playing = input("Are you ready to start", name , "?" "[YES/NO]" )
+if playing.lower() != "yes":
+    quit()
 
-print("Welcome to Flags of the World Quiz!")
+print("Okay!", name, "So Let's start! ")
+score = 0
 
-playing = input("Are you ready to start? [YES/NO]" /n)
+answer = input("A red circle on a green background is a flag of ")
+if answer.lower() == "Bangladesh" or "People's Republic of Bangladesh" :
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
