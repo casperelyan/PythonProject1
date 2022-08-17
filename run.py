ce8440047c3a63c981b45545398e04287145707a
Lucas <4h4photography@gmail.com>
2022-08-17 10:10:07 +0000

    Firts code line commit :)

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+print("Welcome to Flags of the World Quiz!")
+
+playing = input("Are you ready to start? [YES/NO]" /n)
